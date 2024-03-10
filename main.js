f2e1308aea1fcf8a900c92ae16e2ed8cef967bfe
JanHamas <hamasjan82@gmail.com>
2024-03-10 08:24:05 +0500

    df

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
-function copyMenu() {
-  // copy inside .dpt-cat to .department
-  const dptcategory = document.querySelector(".dpt-cat");
-  const departmentplace = document.querySelector(".off-canvas .department");
-  if (dptcategory && departmentplace) {
-    departmentplace.innerHTML = dptcategory.innerHTML;
-  }
+// function copyMenu() {
+//   // copy inside .dpt-cat to .department
+//   const dptcategory = document.querySelector(".dpt-cat");
+//   const departmentplace = document.querySelector(".off-canvas .department");
+//   if (dptcategory && departmentplace) {
+//     departmentplace.innerHTML = dptcategory.innerHTML;
+//   }
 
-  //copy inside nav to nav
-  const mainNav = document.querySelector(".header-nav nav");
-  const navPlace = document.querySelector(".off-canvas nav");
-  if (mainNav && navPlace) {
-    navPlace.innerHTML = mainNav.innerHTML;
-  }
+//   //copy inside nav to nav
+//   const mainNav = document.querySelector(".header-nav nav");
+//   const navPlace = document.querySelector(".off-canvas nav");
+//   if (mainNav && navPlace) {
+//     navPlace.innerHTML = mainNav.innerHTML;
+//   }
 
-  //copy .header-top .wrapper to .thetop-nav
-  const topNav = document.querySelector(".header-top .wrapper");
-  const topNavPlace = document.querySelector("off-canvas .thetop-nav");
-  if (topNav && topNavPlace) {
-    topNavPlace.innerHTML = topNav.innerHTML;
-  }
-}
-copyMenu();
+//   //copy .header-top .wrapper to .thetop-nav
+//   const topNav = document.querySelector(".header-top .wrapper");
+//   const topNavPlace = document.querySelector("off-canvas .thetop-nav");
+//   if (topNav && topNavPlace) {
+//     topNavPlace.innerHTML = topNav.innerHTML;
+//   }
+// }
+// copyMenu();
