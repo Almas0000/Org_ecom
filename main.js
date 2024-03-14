b2f74e198fd2aac302007adf86208e120b76f3ed
Almas000 <hamasjan33@gmail.com>
2024-03-14 06:49:57 +0500

    left: 0;

--- main.js
+++ main.js
@@ -15,3 +15,41 @@ function copyMenu() {
   topPlace.innerHTML = topNav.innerHTML;
 }
 copyMenu();
+
+// show mobile menu
+const menuButton = document.querySelector(".trigger");
+const closeButton = document.querySelector(".t-close");
+const siteElement = document.querySelector(".site");
+
+menuButton.addEventListener("click", function () {
+  siteElement.classList.toggle("showmenu");
+});
+
+closeButton.addEventListener("click", function () {
+  siteElement.classList.remove("showmenu");
+});
+// Show sub menu on mobile
+const subMenus = document.querySelectorAll(".has-child .icon-small");
+subMenus.forEach((menu) => menu.addEventListener("click", toggle));
+
+function toggle(e) {
+  e.preventDefault();
+  subMenus.forEach((item) => {
+    if (item !== this) {
+      item.closest(".has-child").classList.remove("expand");
+    }
+  });
+
+  if (this.closest(".has-child").classList.contains("expand")) {
+    this.closest(".has-child").classList.remove("expand");
+  } else {
+    this.closest(".has-child").classList.add("expand");
+  }
+}
+// import swiper
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
